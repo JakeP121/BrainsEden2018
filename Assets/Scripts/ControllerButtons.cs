0208e939065251b04cafeaa277ce01da884646f6
BrainsEden122 <BrainsEden122@ANGLIA.LOCAL>
2018-07-13 22:08:33 +0100

    Updated input settings and started on controller scripts

---
+++ Assets/Scripts/ControllerButtons.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerButtons : MonoBehaviour {
+
+    private bool up, down, left, right;
+
+    public int dpadValue()
+    {
+        if (DpadLeft())
+            return 1;
+        if (DpadUp())
+            return 2;
+        if (DpadRight())
+            return 3;
+        return 0;
+    }
+
+    bool DpadUp()
+    {
+        if (Input.GetAxisRaw("DPadVertical") == 1 && !up)
+        {
+            StartCoroutine(resetUpBool(0.5f));
+            up = true;
+            return true;
+        }
+        return false;
+    }
+    bool DpadDown()
+    {
+        if (Input.GetAxisRaw("DPadVertical") == -1 && !down)
+        {
+            StartCoroutine(resetDownBool(0.5f));
+            down = true;
+            return true;
+        }
+        return false;
+    }
+    bool DpadLeft()
+    {
+        if (Input.GetAxisRaw("DPadHorizontal") == -1 && !left)
+        {
+            StartCoroutine(resetLeftBool(0.5f));
+            left = true;
+            return true;
+        }
+        return false;
+    }
+    bool DpadRight()
+    {
+        if (Input.GetAxisRaw("DPadHorizontal") == 1 && !right)
+        {
+            StartCoroutine(resetRightBool(0.5f));
+            right = true;
+            return true;
+        }
+        return false;
+    }
+
+    IEnumerator resetLeftBool(float seconds)
+    {
+        float ResumeTime = Time.realtimeSinceStartup + seconds;
+        while (Time.realtimeSinceStartup < ResumeTime)
+        {
+            yield return null;
+        }
+        left = false;
+    }
+    IEnumerator resetRightBool(float seconds)
+    {
+        float ResumeTime = Time.realtimeSinceStartup + seconds;
+        while (Time.realtimeSinceStartup < ResumeTime)
+        {
+            yield return null;
+        }
+        right = false;
+    }
+    IEnumerator resetUpBool(float seconds)
+    {
+        float ResumeTime = Time.realtimeSinceStartup + seconds;
+        while (Time.realtimeSinceStartup < ResumeTime)
+        {
+            yield return null;
+        }
+        up = false;
+    }
+    IEnumerator resetDownBool(float seconds)
+    {
+        float ResumeTime = Time.realtimeSinceStartup + seconds;
+        while (Time.realtimeSinceStartup < ResumeTime)
+        {
+            yield return null;
+        }
+        down = false;
+    }
+}
---
+++ Assets/Scripts/Vibrations.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vibrations : MonoBehaviour {
+
+    private int playerName;
+    private ControllerButtons controller;
+    private int nudge;
+    private int target;
+
+	// Use this for initialization
+	void Start () {
+        playerName = int.Parse(this.gameObject.name);
+        controller = this.GetComponent<ControllerButtons>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        detect();
+        if (selectionMade())
+        {
+            //pass to gameobject or game controller
+        }
+    }
+
+    public void detect()
+    {
+         nudge = controller.dpadValue();
+    }
+
+    private bool selectionMade()
+    {
+        if (nudge != 0 && target != 0)
+            return true;
+        else
+            return false;
+    }
+}
