2ad5dfa147b5584712dead55f7bd6d2a0a7593c9
Steve Sparkes <contact@stevesparkes.com>
2018-07-14 14:37:07 +0100

    Split players can now be displayed on split splash
    Split players can now be displayed on split splash.

--- Assets/Scripts/UISpawnTest.cs
+++ Assets/Scripts/UISpawnTest.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class UISpawnTest : MonoBehaviour {
 
     public GameObject splash_Win;
+    private string split_01 = "Player 1";
+    private string split_02 = "Player 56";
 
     private void Awake()
     {
         GameObject tempSplash = Instantiate(splash_Win);
-        tempSplash.GetComponent<SplashScript>().SetWinner("Player 3 wins!");
+        //tempSplash.GetComponent<SplashScript>().SetWinner("Player 3 wins!");
+        tempSplash.GetComponent<SplashScript>().SetWinner( split_01 + "\n             " + split_02);
         GameObject canvas = GameObject.Find("Canvas");
         tempSplash.transform.SetParent(canvas.transform, false);
     }
