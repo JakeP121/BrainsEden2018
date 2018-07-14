2e42b6ead4a4c47c603bb7f8745e601b5ed8ad31
Steve Sparkes <contact@stevesparkes.com>
2018-07-14 12:15:34 +0100

    Created splash script
    Created splash script.

---
+++ Assets/Scripts/SplashScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SplashScript : MonoBehaviour {
+
+    public Text text;
+
+	public void SetWinner(string name)
+    {
+        text.text = name;
+    }
+
+}
